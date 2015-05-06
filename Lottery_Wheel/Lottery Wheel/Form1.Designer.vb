53a9af6b38d648c586702cc1ce21425a0bdd9a40
uGuardian <uGuardian@users.noreply.github.com>
2015-05-06 13:43:57 -0600

    Add Lottery Wheel files

---
+++ Lottery_Wheel/Lottery Wheel/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.pic3_3 = New System.Windows.Forms.PictureBox()
+        Me.pic2_3 = New System.Windows.Forms.PictureBox()
+        Me.pic1_3 = New System.Windows.Forms.PictureBox()
+        Me.picLever = New System.Windows.Forms.PictureBox()
+        Me.timLever = New System.Windows.Forms.Timer(Me.components)
+        Me.timSpin = New System.Windows.Forms.Timer(Me.components)
+        Me.imlSlots = New System.Windows.Forms.ImageList(Me.components)
+        Me.pic3_1 = New System.Windows.Forms.PictureBox()
+        Me.pic2_1 = New System.Windows.Forms.PictureBox()
+        Me.pic1_1 = New System.Windows.Forms.PictureBox()
+        Me.pic3_2 = New System.Windows.Forms.PictureBox()
+        Me.pic2_2 = New System.Windows.Forms.PictureBox()
+        Me.pic1_2 = New System.Windows.Forms.PictureBox()
+        Me.timSpinning = New System.Windows.Forms.Timer(Me.components)
+        Me.timStart = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.pic3_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1_3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picLever, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1_1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic3_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic2_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pic1_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(12, 630)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(612, 39)
+        Me.btnQuit.TabIndex = 9
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'pic3_3
+        '
+        Me.pic3_3.Location = New System.Drawing.Point(424, 424)
+        Me.pic3_3.Name = "pic3_3"
+        Me.pic3_3.Size = New System.Drawing.Size(200, 200)
+        Me.pic3_3.TabIndex = 7
+        Me.pic3_3.TabStop = False
+        '
+        'pic2_3
+        '
+        Me.pic2_3.Location = New System.Drawing.Point(218, 424)
+        Me.pic2_3.Name = "pic2_3"
+        Me.pic2_3.Size = New System.Drawing.Size(200, 200)
+        Me.pic2_3.TabIndex = 6
+        Me.pic2_3.TabStop = False
+        '
+        'pic1_3
+        '
+        Me.pic1_3.Location = New System.Drawing.Point(12, 424)
+        Me.pic1_3.Name = "pic1_3"
+        Me.pic1_3.Size = New System.Drawing.Size(200, 200)
+        Me.pic1_3.TabIndex = 5
+        Me.pic1_3.TabStop = False
+        '
+        'picLever
+        '
+        Me.picLever.InitialImage = Global.Lottery_Wheel.My.Resources.Resources.lever
+        Me.picLever.Location = New System.Drawing.Point(630, 207)
+        Me.picLever.Name = "picLever"
+        Me.picLever.Size = New System.Drawing.Size(150, 225)
+        Me.picLever.TabIndex = 10
+        Me.picLever.TabStop = False
+        '
+        'timLever
+        '
+        Me.timLever.Interval = 1000
+        '
+        'timSpin
+        '
+        '
+        'imlSlots
+        '
+        Me.imlSlots.ImageStream = CType(resources.GetObject("imlSlots.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.imlSlots.TransparentColor = System.Drawing.Color.Transparent
+        Me.imlSlots.Images.SetKeyName(0, "Banana Peel Triple.png")
+        Me.imlSlots.Images.SetKeyName(1, "Banana Peel.png")
+        Me.imlSlots.Images.SetKeyName(2, "Blooper.png")
+        Me.imlSlots.Images.SetKeyName(3, "Bob-omb.png")
+        Me.imlSlots.Images.SetKeyName(4, "Bullet Bill.png")
+        Me.imlSlots.Images.SetKeyName(5, "Fake Item Box.png")
+        Me.imlSlots.Images.SetKeyName(6, "Golden Mushroom.png")
+        Me.imlSlots.Images.SetKeyName(7, "Green Koopa Shell Triple.png")
+        Me.imlSlots.Images.SetKeyName(8, "Green Koopa Shell.png")
+        Me.imlSlots.Images.SetKeyName(9, "Lightning.png")
+        Me.imlSlots.Images.SetKeyName(10, "Mushroom.png")
+        Me.imlSlots.Images.SetKeyName(11, "Red Koopa Shell Triple.png")
+        Me.imlSlots.Images.SetKeyName(12, "Red Koopa Shell.png")
+        Me.imlSlots.Images.SetKeyName(13, "Star.png")
+        Me.imlSlots.Images.SetKeyName(14, "Triple Mushroom.png")
+        Me.imlSlots.Images.SetKeyName(15, "Winged Spiny Shell.png")
+        '
+        'pic3_1
+        '
+        Me.pic3_1.Location = New System.Drawing.Point(424, 12)
+        Me.pic3_1.Name = "pic3_1"
+        Me.pic3_1.Size = New System.Drawing.Size(200, 200)
+        Me.pic3_1.TabIndex = 13
+        Me.pic3_1.TabStop = False
+        '
+        'pic2_1
+        '
+        Me.pic2_1.Location = New System.Drawing.Point(218, 12)
+        Me.pic2_1.Name = "pic2_1"
+        Me.pic2_1.Size = New System.Drawing.Size(200, 200)
+        Me.pic2_1.TabIndex = 12
+        Me.pic2_1.TabStop = False
+        '
+        'pic1_1
+        '
+        Me.pic1_1.Location = New System.Drawing.Point(12, 12)
+        Me.pic1_1.Name = "pic1_1"
+        Me.pic1_1.Size = New System.Drawing.Size(200, 200)
+        Me.pic1_1.TabIndex = 11
+        Me.pic1_1.TabStop = False
+        '
+        'pic3_2
+        '
+        Me.pic3_2.Location = New System.Drawing.Point(424, 218)
+        Me.pic3_2.Name = "pic3_2"
+        Me.pic3_2.Size = New System.Drawing.Size(200, 200)
+        Me.pic3_2.TabIndex = 16
+        Me.pic3_2.TabStop = False
+        '
+        'pic2_2
+        '
+        Me.pic2_2.Location = New System.Drawing.Point(218, 218)
+        Me.pic2_2.Name = "pic2_2"
+        Me.pic2_2.Size = New System.Drawing.Size(200, 200)
+        Me.pic2_2.TabIndex = 15
+        Me.pic2_2.TabStop = False
+        '
+        'pic1_2
+        '
+        Me.pic1_2.Location = New System.Drawing.Point(12, 218)
+        Me.pic1_2.Name = "pic1_2"
+        Me.pic1_2.Size = New System.Drawing.Size(200, 200)
+        Me.pic1_2.TabIndex = 14
+        Me.pic1_2.TabStop = False
+        '
+        'timSpinning
+        '
+        '
+        'timStart
+        '
+        Me.timStart.Interval = 500
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(789, 681)
+        Me.Controls.Add(Me.pic3_2)
+        Me.Controls.Add(Me.pic2_2)
+        Me.Controls.Add(Me.pic1_2)
+        Me.Controls.Add(Me.pic3_1)
+        Me.Controls.Add(Me.pic2_1)
+        Me.Controls.Add(Me.pic1_1)
+        Me.Controls.Add(Me.picLever)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.pic3_3)
+        Me.Controls.Add(Me.pic2_3)
+        Me.Controls.Add(Me.pic1_3)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pic3_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1_3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picLever, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1_1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic3_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic2_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pic1_2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnQuit As System.Windows.Forms.Button
+    Friend WithEvents pic3_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic2_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic1_3 As System.Windows.Forms.PictureBox
+    Friend WithEvents picLever As System.Windows.Forms.PictureBox
+    Friend WithEvents timLever As System.Windows.Forms.Timer
+    Friend WithEvents timSpin As System.Windows.Forms.Timer
+    Friend WithEvents imlSlots As System.Windows.Forms.ImageList
+    Friend WithEvents pic3_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic2_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic1_1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic3_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic2_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pic1_2 As System.Windows.Forms.PictureBox
+    Friend WithEvents timSpinning As System.Windows.Forms.Timer
+    Friend WithEvents timStart As System.Windows.Forms.Timer
+
+End Class
---
+++ Lottery_Wheel/Lottery Wheel/Form1.vb
@@ -0,0 +1,188 @@
+﻿Public Class Form1
+    Dim Slot1_1 As Integer
+    Dim Slot1_2 As Integer
+    Dim Slot1_3 As Integer
+    Dim Slot2_1 As Integer
+    Dim Slot2_2 As Integer
+    Dim Slot2_3 As Integer
+    Dim Slot3_1 As Integer
+    Dim Slot3_2 As Integer
+    Dim Slot3_3 As Integer
+    Dim Slot1Stop As Integer
+    Dim Slot2Stop As Integer
+    Dim Slot3Stop As Integer
+    Dim Started As Integer
+
+    Private Sub picLever_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles picLever.Click
+        picLever.Image = My.Resources.lever_pulled
+        If Started = 0 Then
+            timStart.Start()
+            Started = 1
+            Audio()
+        Else : timSpin.Interval = Int(Rnd() * 2000)
+            timSpin.Start()
+        End If
+        timLever.Start()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        picLever.Image = My.Resources.lever
+        Slot1_1 = 0
+        Slot1_2 = 1
+        Slot1_3 = 2
+
+        Slot2_1 = 0
+        Slot2_2 = 1
+        Slot2_3 = 2
+
+        Slot3_1 = 0
+        Slot3_2 = 1
+        Slot3_3 = 2
+
+        Slot1Stop = 0
+        Slot2Stop = 0
+        Slot3Stop = 0
+
+        pic1_1.Image = imlSlots.Images.Item(Slot1_1)
+        pic1_2.Image = imlSlots.Images.Item(Slot1_2)
+        pic1_3.Image = imlSlots.Images.Item(Slot1_3)
+
+        pic2_1.Image = imlSlots.Images.Item(Slot2_1)
+        pic2_2.Image = imlSlots.Images.Item(Slot2_2)
+        pic2_3.Image = imlSlots.Images.Item(Slot2_3)
+
+        pic3_1.Image = imlSlots.Images.Item(Slot3_1)
+        pic3_2.Image = imlSlots.Images.Item(Slot3_2)
+        pic3_3.Image = imlSlots.Images.Item(Slot3_3)
+
+        My.Computer.Audio.Play("U:\Lottery Wheel\Lottery Wheel\Resources\Halloween_MainTheme_converted.wav")
+        My.Computer.Audio.Stop()
+    End Sub
+
+    Private Sub btnQuit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnQuit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub timlever_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timLever.Tick
+        picLever.Image = My.Resources.lever
+        timLever.Stop()
+    End Sub
+
+    Private Sub timSpin_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timSpin.Tick
+        If Slot1Stop = 0 Then
+            Slot1Stop = 1
+        ElseIf Slot2Stop = 0 Then
+            Slot2Stop = 1
+        ElseIf Slot3Stop = 0 Then
+            Slot3Stop = 1
+            Started = 0
+            Audio()
+            Winner()
+        End If
+
+        timSpin.Stop()
+    End Sub
+
+    Private Sub timSpinning_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timSpinning.Tick
+        If Slot1Stop = 0 Then
+            Slot1_1 = Slot1_1 + 1
+            Slot1_2 = Slot1_2 + 1
+            Slot1_3 = Slot1_3 + 1
+        End If
+        If Slot2Stop = 0 Then
+            Slot2_1 = Slot2_1 + 1
+            Slot2_2 = Slot2_2 + 1
+            Slot2_3 = Slot2_3 + 1
+        End If
+        If Slot3Stop = 0 Then
+            Slot3_1 = Slot3_1 + 1
+            Slot3_2 = Slot3_2 + 1
+            Slot3_3 = Slot3_3 + 1
+        End If
+
+        If Slot1_1 > 15 Then
+            Slot1_1 = 0
+        End If
+        If Slot1_2 > 15 Then
+            Slot1_2 = 0
+        End If
+        If Slot1_3 > 15 Then
+            Slot1_3 = 0
+        End If
+
+        If Slot2_1 > 15 Then
+            Slot2_1 = 0
+        End If
+        If Slot2_2 > 15 Then
+            Slot2_2 = 0
+        End If
+        If Slot2_3 > 15 Then
+            Slot2_3 = 0
+        End If
+
+        If Slot3_1 > 15 Then
+            Slot3_1 = 0
+        End If
+        If Slot3_2 > 15 Then
+            Slot3_2 = 0
+        End If
+        If Slot3_3 > 15 Then
+            Slot3_3 = 0
+        End If
+
+        pic1_1.Image = imlSlots.Images.Item(Slot1_1)
+        pic1_2.Image = imlSlots.Images.Item(Slot1_2)
+        pic1_3.Image = imlSlots.Images.Item(Slot1_3)
+
+        pic2_1.Image = imlSlots.Images.Item(Slot2_1)
+        pic2_2.Image = imlSlots.Images.Item(Slot2_2)
+        pic2_3.Image = imlSlots.Images.Item(Slot2_3)
+
+        pic3_1.Image = imlSlots.Images.Item(Slot3_1)
+        pic3_2.Image = imlSlots.Images.Item(Slot3_2)
+        pic3_3.Image = imlSlots.Images.Item(Slot3_3)
+    End Sub
+
+    Private Sub timStart_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles timStart.Tick
+        timSpinning.Start()
+        If Slot1Stop = 1 Then
+            Slot1Stop = 0
+        ElseIf Slot2Stop = 1 Then
+            Slot2Stop = 0
+        ElseIf Slot3Stop = 1 Then
+            Slot3Stop = 0
+        Else : timStart.Stop()
+        End If
+    End Sub
+
+    Private Sub Audio()
+        If Started = 1 Then
+            My.Computer.Audio.Play("U:\Lottery Wheel\Lottery Wheel\Resources\Halloween_MainTheme.wav")
+        Else : My.Computer.Audio.Stop()
+        End If
+    End Sub
+
+    Private Sub Winner()
+        pic1_1.Image = imlSlots.Images.Item(Slot1_1)
+        pic1_2.Image = imlSlots.Images.Item(Slot1_2)
+        pic1_3.Image = imlSlots.Images.Item(Slot1_3)
+
+        pic2_1.Image = imlSlots.Images.Item(Slot2_1)
+        pic2_2.Image = imlSlots.Images.Item(Slot2_2)
+        pic2_3.Image = imlSlots.Images.Item(Slot2_3)
+
+        pic3_1.Image = imlSlots.Images.Item(Slot3_1)
+        pic3_2.Image = imlSlots.Images.Item(Slot3_2)
+        pic3_3.Image = imlSlots.Images.Item(Slot3_3)
+
+        If Slot1_2 = 15 And Slot2_2 = 15 And Slot3_2 = 15 Then
+            My.Computer.Audio.Play("U:\Lottery Wheel\Lottery Wheel\Resources\explosion-01.wav")
+            MessageBox.Show("BOOM! HEADSHOT!")
+            My.Computer.Audio.Stop()
+        ElseIf Slot1_2 = Slot2_2 And Slot2_2 = Slot3_2 And Slot1_2 = Slot3_2 Then
+            My.Computer.Audio.Play("U:\Lottery Wheel\Lottery Wheel\Resources\JackPotWinMusic.wav")
+            MessageBox.Show("You win!")
+            My.Computer.Audio.Stop()
+        End If
+    End Sub
+End Class
